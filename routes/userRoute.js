e20d40348899540570cac20b473733526b30a4b7
taeannesarai <gtaeanne@gmail.com>
2024-02-21 10:18:26 -0500

    ...

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import BodyParser from "body-parser";
-import {getAlluser, saveUser, updateUser, getSingleUser, deleteUser  } from "../data/database.js";
+import {getAllUsers, saveUser, updateUser, getSingleUser, deleteUser  } from "../data/database.js";
 
 const router = express.Router();
 const app = express();
@@ -8,21 +8,20 @@ const app = express();
 app.use(BodyParser.urlencoded({ extended: true }));
 
 
-
 //route for home page ------------ path: '/'
 router.get("/", async (req, res) => {
   res.render("index")
 });
 
+// student users
+
+// route for to view all users
 router.get("/all-users", async (req, res) => {
-  const results = await getAllReservations();
+  const results = await getAllUsers();
   console.log(results);
-  res.render("reservations/all-reservations", {
-    title: "All Reservations",
-    resData: results,
-  });
+  res.render("users/all-users", {title: "All users", resData: results});
 });
 
 
 //! DO NOT CREATE ANY ROUTES BELOW THIS EXPORT
-export const loginRoute = router;
+export const userRoute = router;
