2bd2044baa30f9efa586196a997c953c9fc27aaa
jev-16 <jevardo99williamson@gmail.com>
2024-02-27 13:28:53 -0500

    Function added to check password length and if noth password fields are the same on the signup page

--- util/script.js
+++ util/script.js
@@ -4,15 +4,15 @@ let btnToTop = document.querySelector(".to-top-btn");
 
 window.onscroll = () => {
 	const toTopStyle = {
-        display: "block",
-        transform: 'scale'
+		display: "block",
+		transform: "scale",
 	};
 	if (document.documentElement.scrollTop > 25) {
 		navBar.style.backgroundColor = "#fff";
 		logoImg.classList.add("logoStyle");
 		btnToTop.classList.add("not-at-top");
 	} else {
-        navBar.style.backgroundColor = "none";
+		navBar.style.backgroundColor = "none";
 		logoImg.classList.remove("logoStyle");
 		btnToTop.classList.remove("not-at-top");
 	}
@@ -22,15 +22,31 @@ function toTop() {
 	scrollTo(0, 0);
 }
 
-// CHECK IF USER PASWORD ENTIES ARE THE SAME
-let userPW = document.querySelector("user-pw");
-let userPWConfirm = document.querySelector("user-pw-confirm");
-let signupBtn = document.querySelector("signup-submit-btn");
+// CHECK IF USER PASSWORD ENTRIES ARE THE SAME
+function isPWMatch() {
+	let userPW = document.querySelector(".user-pw");
+	let userPWConfirm = document.querySelector(".user-pw-confirm");
+	let signupBtn = document.querySelector(".signup-submit-btn");
 
-userPWConfirm.addEventListener('keyup', () => {
-	if (userPWConfirm.value > 0 && userPW.value == userPWConfirm.value) {
-		signupBtn.setAttribute(disabled, false);
-	} else {
-		signupBtn.setAttribute(disabled, true);
-	}
-})
+	signupBtn.setAttribute("disabled", "");
+
+	userPW.addEventListener("keyup", () => {
+		if (userPW.value.length < 7) {
+			userPWConfirm.setAttribute("disabled", "");
+		} else {
+			userPWConfirm.removeAttribute("disabled");
+		}
+	});
+
+	userPWConfirm.addEventListener("keyup", () => {
+		if (userPWConfirm.value.length > 7 && userPW.value.length > 7 && userPW.value == userPWConfirm.value) {
+			signupBtn.removeAttribute("disabled");
+			console.log("Match");
+		} else {
+			signupBtn.setAttribute("disabled", "");
+			console.log("!Match");
+		}
+	});
+}
+
+isPWMatch();
